551b0feb7fcea38ce5593ebec90f25f188e1943b
Jyoti Bharti <76005177+stackpointer1@users.noreply.github.com>
2021-05-04 22:30:04 +0530

    init

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { useEffect, useState } from "react";
 export default function App() {
   // const api=(limit,page)`https://jsonplaceholder.typicode.com/posts?_limits=${limit} &_page=${page}`
   const [dataArray, setDataArray] = useState([]);
-  const[pageNumber,setPageNumber]=useState(1)
-  
+  const [pageNumber, setPageNumber] = useState(1);
+
   useEffect(() => {
     const apicallFunction = async () => {
       const apiData = await fetch(
@@ -18,11 +18,15 @@ export default function App() {
     };
     apicallFunction();
   }, [pageNumber]);
+  const MAX_PAGE_Number = 20;
+  const pageNumberArray = new Array(MAX_PAGE_Number)
+    .fill(1)
+    .map((_, idx) => idx + 1);
   const handlePrev = () => {
-    setPageNumber(pageNumber-1);
+    setPageNumber(pageNumber <= 1 ? pageNumber : pageNumber - 1);
   };
   const handleNext = () => {
-    setPageNumber(pageNumber+1);
+    setPageNumber(pageNumber >= 20 ? pageNumber : pageNumber + 1);
   };
   return (
     <div className="App">
@@ -31,8 +35,24 @@ export default function App() {
           <li>{data.id}</li>
         ))}
       </ul>
-      <button onClick={handlePrev}>previous</button>
-      <button onClick={handleNext}>Next</button>
+      <button disabled={pageNumber === 1} onClick={handlePrev}>
+        previous
+      </button>
+      <button disabled={pageNumber <= 0} onClick={handleNext}>
+        Next
+      </button>
+      <div>
+        {pageNumberArray.map((item, idx) => (
+          <button
+            key={idx}
+            onClick={() => {
+              setPageNumber(idx + 1);
+            }}
+          >
+            {item}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
